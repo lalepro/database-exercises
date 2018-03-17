3928520f750f40d76549681dd7f440e88d4077bc
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-17 10:18:18 -0500

    working on db exercise with base statements

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,8 +4,8 @@ select * from albums where artist = 'pink floyd';
 
 select release_date where name = 'Sgt. Peppers Lonely Hearts Club Band';
 
-SELECT genre where name = 'Nevermind';
+SELECT genre from albums where name = 'Nevermind';
 
-select albums where YEAR >=1990 && YEAR < 2000;
+select * from albums where YEAR >=1990 && YEAR < 2000;
 
-select albums
+select * from albums where genre = 'rock';
