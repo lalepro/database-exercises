7a17a14d6982409d80be474515d4c8e15a758b66
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-18 16:59:07 -0500

    finished basic statements

---
+++ delete_exercises.sql
@@ -0,0 +1,8 @@
+-- SELECT * from albums WHERE release_date >= 1991;
+DELETE FROM albums WHERE release_date >= 1991;
+
+-- SELECT * from albums where genre = disco;
+DELETE FROM albums WHERE genre = 'disco';
+
+-- select * from albums where artist = 'nirvana';
+DELETE FROM albums WHERE artist = 'nirvana';
