2c337f8c046d28379758468c5b8a6c4a4eeeae17
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-18 16:42:57 -0500

    save excersie

--- update_exercise.sql
+++ update_exercise.sql
@@ -1,8 +1,12 @@
-SELECT * albums;
+SELECT * from albums;
 UPDATE albums set sales = (sales * 10);
+SELECT * from albums;
 
+SELECT * from albums where release_date <= 1980;
+UPDATE albums set release_date = 1800 where release_date <= 1980;
 SELECT * from albums where release_date <= 1980;
 
 SELECT * from albums where artist = 'Michael Jackson';
-
+UPDATE albums set artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT * from albums where artist = 'Peter Jackson';
 
