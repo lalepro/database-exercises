30b2a646c17add39058945dfa2c16cb2467f6b8f
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-19 11:29:00 -0500

    added info from Justin's walk through

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,14 +2,20 @@ USE codeup_test_db;
 
 select * from albums where artist = 'pink floyd';
 
-select release_date where name = 'Sgt. Peppers Lonely Hearts Club Band';
+-- select * from albums where artist = 'pink floyd' AS 'exercise 1';
+
+select release_date from albums where name = 'Sgt. Peppers Lonely Hearts Club Band';
 
 SELECT genre from albums where name = 'Nevermind';
 
-select * from albums where release_date >=1990 && YEAR < 2000;
+select * from albums where release_date >=1990 && release_date < 2000;
 
-select * from ablums WHERE release_date BETWEEN 1990 and 2000;
+-- SELECT 'which albums where released in the 90s' AS 'exercise 4';
+select * from albums WHERE release_date BETWEEN 1990 and 1999;
 
-select * from albums where genre = 'rock';
+-- select 'Where albums had less than 20 million certified sales' as 'Exercise 5';
+select * from albums where sales < 20;
+
+select name,genre from albums where genre = 'rock';
 
 
