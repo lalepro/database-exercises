fc41caee027be7c1b401726cedff21de3e32fc43
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-19 11:28:12 -0500

    added justin's walk through

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,13 +1,28 @@
 USE codeup_test_db;
 
-SELECT * from albums;
-UPDATE albums set sales = (sales * 10);
+-- step 1
 SELECT * from albums;
 
+UPDATE albums
+set sales = (sales * 10);
+
+SELECT * from albums;
+
+-- step 2
+SELECT name,release_date from albums where release_date <= 1980;
+
+UPDATE albums
+set release_date = release_date - 100
+where release_date < 1980;
+
 SELECT * from albums where release_date <= 1980;
-UPDATE albums set release_date = 1800 where release_date <= 1980;
-SELECT * from albums where release_date <= 1980;
+
+-- step 3
 
 SELECT * from albums where artist = 'Michael Jackson';
-UPDATE albums set artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+
+UPDATE albums
+set artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+
 SELECT * from albums where artist = 'Peter Jackson';
