a13fdaa0a60f8c739cf0718d43e8a66423552981
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-19 10:42:32 -0500

    learned about BETWEEN

--- select_exercises.sql
+++ select_exercises.sql
@@ -6,7 +6,9 @@ select release_date where name = 'Sgt. Peppers Lonely Hearts Club Band';
 
 SELECT genre from albums where name = 'Nevermind';
 
-select * from albums where YEAR >=1990 && YEAR < 2000;
+select * from albums where release_date >=1990 && YEAR < 2000;
+
+select * from ablums WHERE release_date BETWEEN 1990 and 2000;
 
 select * from albums where genre = 'rock';
 
