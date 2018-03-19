de605ac37b4050c5dcd1f1133456aa49c242ed85
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-19 11:53:49 -0500

    save changes:

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -2,8 +2,9 @@ USE codeup_test_db;
 TRUNCATE TABLE albums;
 -- insert into albums (artist, name, release_date, sales, genre) VALUES ('Michael Jackson', 'Thriller', 1982, 66, 'Pop, Rock, R&B');
 insert into albums (artist, name, release_date, sales, genre)
-VALUES ('AD/DC', 'Back in Black', 1980, 47.3, 'Hard rock'),
-      ('Pink Floyd', 'The Dark Side of the Moon', 1973, 26.1, 'Progressive rock'),
+VALUES
+  ('AD/DC', 'Back in Black', 1980, 47.3, 'Hard rock'),
+  ('Pink Floyd', 'The Dark Side of the Moon', 1973, 26.1, 'Progressive rock'),
   ('Meat Loaf', 'Bat Out of Hell', 1977, 21.5, 'Hard rock, Progressive rock'),
   ('Whitney Houston / Various Artists', 'The Bodyguard', 1992, 28.4, 'R&B, Soul, Pop, Soundtrack'),
   ('Eagles', 'The Greatest Hits (1971- 1975)', 1976, 32.2, 'Rock, Soft rock, Folk rock'),
--- bonus_db.sql
+++ bonus_db.sql
@@ -1,9 +1,10 @@
+use bonus_db;
 drop table if exists products;
 create table if not exists products(
  id int unsigned not null auto_increment,
   VARCHAR(120),
  name VARCHAR (120),
  price DECIMAL(5,2),
- isSold (tiny)
+ isSold (tinyint),
  PRIMARY KEY (id)
 );
