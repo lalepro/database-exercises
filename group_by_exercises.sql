c9ae84156442c3f0cff2568d269d67960b18acfc
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-20 14:05:51 -0500

    finished exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -17,6 +17,25 @@ GROUP BY hire_date
 ORDER BY COUNT(*) DESC
 LIMIT 10;
 
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE employees < 20000;
+
+SELECT DISTINCT first_name
+FROM employees;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
+GROUP BY last_name
+ORDER BY last_name;
+
+
+
+
+
+
+
 
 # GROUP BY EXERCISES
 
@@ -28,6 +47,48 @@ SELECT DISTINCT title FROM employees.titles;
 # 3 Find your query for employees whose last names start
 # and end with 'E'. Update the query find just the unique
 # last names that start and end with 'E' using GROUP BY.
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
+GROUP BY last_name
+ORDER BY last_name;
+
+# 4 Update your previous query to now find unique combinations
+# of first and last name where the last name starts and ends
+# with 'E'. You should get 846 rows.
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY first_name, last_name
+ORDER BY last_name;
+
+# 5 Find the unique last names with a 'q' but not 'qu'.
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND
+  last_name NOT LIKE '%qu%';
+
+# 6 Add a COUNT() to your results and use ORDER BY to
+# make it easier to find employees whose unusual name
+# is shared with others.
+
+SELECT count(DISTINCT last_name)
+FROM employees
+WHERE last_name LIKE '%q%' AND
+      last_name NOT LIKE '%qu%';
+
+# 7 Update your query for 'Irena', 'Vidya', or 'Maya'.
+# Use count(*) and GROUP BY to find the number of
+# employees for each gender with those names.
+
+SELECT concat(count(first_name), ' ', gender)
+FROM employees
+  WHERE first_name
+        IN ('irena', 'vidya', 'maya')
+GROUP BY gender;
 
 
 
