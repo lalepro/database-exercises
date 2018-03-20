cc05fdedbaddcf52bf8d728816746b5fda972f09
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-19 20:57:36 -0500

    finished order by mysql exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,17 +3,19 @@ USE employees;
 # 1 Create a file named where_exercises.sql. Make sure to use the employees database
 # 2 Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
 SELECT * FROM employees WHERE first_name IN ('irena', 'vidya', 'maya') ORDER BY first_name ASC;
-SELECT first_name, last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name asc;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name asc;
 # OR
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' ORDER BY last_name, first_name ASC;
 # 3 Find all employees whose last name starts with 'E' — 7,330 rows.
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC ;
 # 4 Find all employees hired in the 90s — 135,214 rows.
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31';
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31' ORDER BY birth_date DESC , hire_date DESC ;
 # 5 Find all employees born on Christmas — 842 rows.
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
 # 6 Find all employees with a 'q' in their last name — 1,873 rows.
 SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+
 
 # ----------------------------------------------------------------------------------------------------
 # 1 Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
