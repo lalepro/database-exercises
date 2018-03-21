295453543341d55b222e2ab53644bc8d12e5dcc9
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-20 19:24:35 -0500

    created seeder db for bonus db

--- bonus_db_seeder.sql
+++ bonus_db_seeder.sql
@@ -1,10 +1,14 @@
-use bonus_db;
-drop table if exists products;
-create table if not exists products(
- id int unsigned not null auto_increment,
-  VARCHAR(120),
- name VARCHAR (120),
- price DECIMAL(5,2),
- isSold (tinyint),
- PRIMARY KEY (id)
-);
+USE bonus_db;
+TRUNCATE TABLE products;
+-- insert into albums (artist, name, release_date, sales, genre) VALUES ('Michael Jackson', 'Thriller', 1982, 66, 'Pop, Rock, R&B');
+insert into bonus_db.products (name, price, isSold)
+VALUES
+  ('bike', 250.00, TRUE),
+  ('cat', 100.00, FALSE ),
+  ('bat', 30.00, TRUE),
+  ('fish', 50.00, FALSE ),
+  ('nes', 200.00, TRUE),
+  ('n64', 300.00, FALSE ),
+  ('ps3', 400.00, TRUE ),
+  ('dog', 75.00, FALSE ),
+  ('drill', 60.00, FALSE );
