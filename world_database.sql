8bc5b4d36f5d0e91e93fe635fdca857d7eeeb24a
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-22 10:05:46 -0500

    save completed exercise up to bonus, will work on later

--- world_database.sql
+++ world_database.sql
@@ -4,6 +4,8 @@ SELECT * FROM city WHERE Name = 'santa monica';
 
 # 2 FROM MYSQL EXTRA EXERCISES
 
+# What languages are spoken in Santa Monica?
+
 # JOIN METHOD
 SELECT Language, Percentage
 FROM countrylanguage AS cl
@@ -20,4 +22,40 @@ SELECT Language, Percentage FROM countrylanguage WHERE CountryCode IN (
     SELECT CountryCode FROM city WHERE name = 'santa monica'
   )
 );
+
+# How many different countries are in each region?
+SELECT Region, count(Name) FROM country GROUP BY Region ORDER BY count(Name);
+
+# SELECT Region FROM country GROUP BY Region;
+
+
+# What is the population for each region?
+
+SELECT Region , sum(Population) as sp FROM country GROUP BY Region ORDER BY sp DESC;
+
+
+# What is the population for each continent?
+
+SELECT Continent, sum(Population) as pop FROM country GROUP BY Continent ORDER BY pop DESC ;
+
+# What is the average life expectancy globally?
+
+SELECT avg(LifeExpectancy) FROM country;
+
+SELECT name, max(LifeExpectancy) as le FROM country GROUP BY Name ORDER BY le DESC LIMIT 1;
+
+# What is the average life expectancy for each region, each continent?
+# Sort the results from shortest to longest
+
+SELECT Continent, avg(LifeExpectancy) as le FROM country GROUP BY Continent ORDER BY le;
+
+SELECT Region, avg(LifeExpectancy) AS le FROM country GROUP BY Region ORDER BY le;
+
+SELECT LocalName, Name FROM country WHERE LocalName != Name;
+
+
+
+
+
+
 
